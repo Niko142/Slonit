52c338695028c1c86f147d6e351375b9822b21eb
Niko142 <baya29.03.1977@gmail.com>
2025-02-13 15:15:54 +0400

    add new object methods

---
+++ object.js
@@ -0,0 +1,33 @@
+const user = {
+  name: "Nikita",
+  age: 42,
+  sum: 100,
+  adress: "Back-street",
+  adaw: NaN,
+};
+
+const sortObj = {
+  name: "Vasya",
+  friends: 5,
+  likes: 19,
+  projects: 7,
+};
+
+function getSummNumbers(obj) {
+  let totalSum = 0;
+  for (let i in obj) {
+    if (!isNaN(obj[i])) {
+      totalSum += obj[i];
+    }
+  }
+  return totalSum;
+}
+
+console.log(getSummNumbers(user));
+
+function getSortArr(obj) {
+  const resArr = Object.keys(obj).filter((ind) => !isNaN(obj[ind])).sort((a, b) => obj[b] - obj[a]);
+  return resArr;
+}
+
+console.log(getSortArr(sortObj));
